b1101444f6ad1012b587030afb2a4d1d41e0761c
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2013-05-09 23:51:46 +0100

    Make a setup,py and tag as release 0

---
+++ setup.py
@@ -0,0 +1,23 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'A LMTP counterpart to smtpd in the Python standard library',
+    'author': 'Matt Molyneaux',
+    'url': 'https://github.com/moggers87/lmtpd',
+    'download_url': 'https://github.com/moggers87/lmtpd',
+    'author_email': 'moggers87+git@moggers87.co.uk',
+    'version': '0',
+    'packages': ['lmtpd'],
+    'name': 'lmtpd',
+    'classifiers': [
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2',
+        'Intended Audience :: Developers',
+        'Topic :: Communications :: Email']
+}
+
+setup(**config)
